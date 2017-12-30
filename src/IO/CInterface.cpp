18f19215d94f3f7e4348aff2ff47517020757016
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2017-12-30 17:14:44 -0600

    Prepended superphot to library names + better library interface (#1).

--- src/IO/CInterface.cpp
+++ src/IO/CInterface.cpp
@@ -8,6 +8,20 @@
 #define BUILDING_LIBRARY
 #include "CInterface.h"
 
+const char MASK_OK = Core::MASK_OK;
+const char MASK_CLEAR = Core::MASK_CLEAR;
+const char MASK_FAULT = Core::MASK_FAULT;
+const char MASK_HOT = Core::MASK_HOT;
+const char MASK_COSMIC = Core::MASK_COSMIC;
+const char MASK_OUTER = Core::MASK_OUTER;
+const char MASK_OVERSATURATED = Core::MASK_OVERSATURATED;
+const char MASK_LEAKED = Core::MASK_LEAKED;
+const char MASK_SATURATED = Core::MASK_SATURATED;
+const char MASK_INTERPOLATED = Core::MASK_INTERPOLATED;
+const char MASK_BAD = Core::MASK_BAD;
+const char MASK_ALL = Core::MASK_ALL;
+const char MASK_NAN = Core::MASK_NAN;
+
 void parse_hat_mask(const char *mask_string,
                     long x_resolution,
                     long y_resolution,
--- src/IO/CInterface.h
+++ src/IO/CInterface.h
@@ -7,9 +7,50 @@
  */
 
 #include "../Core/SharedLibraryExportMacros.h"
+#include "../Core/Image.h"
 #include "parse_hat_mask.h"
 
 extern "C" {
+
+    ///See Core::MASK_OK
+    LIB_PUBLIC extern const char MASK_OK;
+
+    ///See Core::MASK_CLEAR
+    LIB_PUBLIC extern const char MASK_CLEAR;
+
+
+    ///See Core::MASK_FAULT
+    LIB_PUBLIC extern const char MASK_FAULT;
+
+    ///See Core::MASK_HOT
+    LIB_PUBLIC extern const char MASK_HOT;
+
+    ///See Core::MASK_COSMIC
+    LIB_PUBLIC extern const char MASK_COSMIC;
+
+    ///See Core::MASK_OUTER
+    LIB_PUBLIC extern const char MASK_OUTER;
+
+    ///See Core::MASK_OVERSATURATED
+    LIB_PUBLIC extern const char MASK_OVERSATURATED;
+
+    ///See Core::MASK_LEAKED
+    LIB_PUBLIC extern const char MASK_LEAKED;
+
+    ///See Core::MASK_SATURATED
+    LIB_PUBLIC extern const char MASK_SATURATED;
+
+    ///See Core::MASK_INTERPOLATED
+    LIB_PUBLIC extern const char MASK_INTERPOLATED;
+
+    ///See Core::MASK_BAD
+    LIB_PUBLIC extern const char MASK_BAD;
+
+    ///See Core::MASK_ALL
+    LIB_PUBLIC extern const char MASK_ALL;
+
+    ///See Core::MASK_NAN
+    LIB_PUBLIC extern const char MASK_NAN;
 
     ///C-binding alias for IO::parse_hat_mask.
     LIB_PUBLIC void parse_hat_mask(const char *mask_string,
