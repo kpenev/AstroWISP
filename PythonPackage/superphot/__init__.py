1bcc74d924b602f16e66729c0e44853bec5fde31
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2020-02-19 14:19:44 -0600

    Fixed typo.

--- PythonPackage/superphot/__init__.py
+++ PythonPackage/superphot/__init__.py
@@ -5,7 +5,7 @@ from superphot.fit_star_shape import FitStarShape
 from superphot.subpixphot import SubPixPhot
 from superphot.smooth_dependence import SmoothDependence
 from superphot.superphot_io_tree import SuperPhotIOTree
-from superphot.piecewiseb_bicubic_psf_map import PiecewiseBicubicPSFMap
+from superphot.piecewise_bicubic_psf_map import PiecewiseBicubicPSFMap
 
 __all__ = ['BackgroundExtractor',
            'FitStarShape',
