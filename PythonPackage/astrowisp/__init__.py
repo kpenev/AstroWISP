c4382ef0cc40f89f3031f9caf91045d35f038ef5
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2024-10-16 20:01:55 -0500

    Add unit test of fistar

--- PythonPackage/astrowisp/__init__.py
+++ PythonPackage/astrowisp/__init__.py
@@ -1,11 +1,18 @@
 """AstroWISP python interface."""
 
+from os import path
+
 from astrowisp.background import BackgroundExtractor
 from astrowisp.fit_star_shape import FitStarShape
 from astrowisp.subpixphot import SubPixPhot
 from astrowisp.io_tree import IOTree
 from astrowisp.piecewise_bicubic_psf_map import PiecewiseBicubicPSFMap
 from astrowisp.piecewise_bicubic_psf import PiecewiseBicubicPSF
+
+_module_path = path.dirname(path.abspath(__file__))
+
+fistar_path = path.join(_module_path, 'fistar')
+grcollect_path = path.join(_module_path, 'grcollect')
 
 __all__ = ['BackgroundExtractor',
            'FitStarShape',
