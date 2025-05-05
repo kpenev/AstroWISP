5d5ab3c88e65d2263e08316432d148972f4e1d93
Kaloyan Penev <kaloyan.penev@utdallas.edu>
2025-05-05 14:53:36 -0500

    A bit more flexible testing utilities

--- PythonPackage/astrowisp/tests/utilities.py
+++ PythonPackage/astrowisp/tests/utilities.py
@@ -7,17 +7,20 @@ from sys import float_info
 class FloatTestCase(unittest.TestCase):
     """Test case involving floating point comparisons."""
 
-    tolerance = 10.0
+    relative_tolerance = 10.0
+    absolute_tolerance = 0.0
 
     def approx_equal(self, value_1, value_2):
         """Return True iff value_1 ~= value_2 to within tolerance."""
 
-        return (
-            abs(value_1 - value_2)
-            <= self.tolerance * abs(value_1 + value_2) * float_info.epsilon
+        return abs(value_1 - value_2) <= (
+            self.relative_tolerance
+            * abs(value_1 + value_2)
+            * float_info.epsilon
+            + self.absolute_tolerance
         )
 
-    def set_tolerance(self, tolerance):
+    def set_tolerance(self, relative, absolute):
         """
         Set the tolerance for floating point comparisons.
 
@@ -30,7 +33,8 @@ class FloatTestCase(unittest.TestCase):
             None
         """
 
-        self.tolerance = tolerance
+        self.relative_tolerance = relative
+        self.absolute_tolerance = absolute
 
     # Following standard unittest assert naming convections
     # pylint: disable=invalid-name
@@ -56,12 +60,15 @@ class FloatTestCase(unittest.TestCase):
 
         self.assertTrue(
             self.approx_equal(value_1, value_2),
-            f"{value_1:f} !~ {value_2:f}: {message}",
+            f"{value_1!r} !~ {value_2!r} (diff {value_1 - value_2}): {message}",
         )
-
 
     def assertApproxPandas(self, expected, testing, testing_what=""):
         """Assert that the two dataframes match columns, types and values."""
+
+        if hasattr(self, '_logger'):
+            self._logger.debug("Expected :\n%s", repr(expected))
+            self._logger.debug("Got :\n%s", repr(testing))
 
         self.assertTrue(
             (
@@ -92,4 +99,5 @@ class FloatTestCase(unittest.TestCase):
                 assert expected_col.dtype.kind == "f"
             for expected_val, testing_val in zip(expected_col, testing_col):
                 self.assertApprox(expected_val, testing_val, mismatch_message)
+
     # pylint: enable=invalid-name
